4bfd63769c1ddb65f11bc4178d8565d0a034bb17
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-15 02:13:36 -0500

    Added logic to determine TicketPrice based on Age

--- main.go
+++ main.go
@@ -11,19 +11,16 @@ type customer struct {
 func main() {
 	customers := []customer{
 		{1, "Sreeja", 25},
-		{2, "Bharath", 16},
+		{2, "Bharath", 12},
 		{3, "Sai", 28},
 		{4, "Raj", 10},
 		{5, "Ravi", 18},
 	}
-	/*for _, c := range customers {
-		fmt.Printf("ID: %d , Name: %s , Age: %d\n", c.ID, c.Name, c.Age)
-	}*/
 	fmt.Printf("%-5s %-10s %-5s %-40s\n", "ID", "Name", "Age", "Status")
 	for _, c := range customers {
 		if c.Age < 12 {
 			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Not Allowed")
-		} else if c.Age > 12 && c.Age < 18 {
+		} else if c.Age >= 12 && c.Age < 18 {
 			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Allowed but must come with someone above 21")
 		} else {
 			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Allowed")
