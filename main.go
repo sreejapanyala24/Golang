269c4ca390260b1a042d0b1fb24562fb102606a5
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-15 01:30:43 -0500

    Added logic for Task1 - Allow users based on age

--- main.go
+++ main.go
@@ -2,22 +2,32 @@ package main
 
 import "fmt"
 
-type Customer struct {
+type customer struct {
 	ID   int
 	Name string
 	Age  int
 }
 
 func main() {
-
-	customers := []Customer{
+	customers := []customer{
 		{1, "Sreeja", 25},
-		{2, "Bharath", 30},
+		{2, "Bharath", 16},
 		{3, "Sai", 28},
-		{4, "Raj", 27},
+		{4, "Raj", 10},
+		{5, "Ravi", 18},
+	}
+	/*for _, c := range customers {
+		fmt.Printf("ID: %d , Name: %s , Age: %d\n", c.ID, c.Name, c.Age)
+	}*/
+	fmt.Printf("%-5s %-10s %-5s %-40s\n", "ID", "Name", "Age", "Status")
+	for _, c := range customers {
+		if c.Age < 12 {
+			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Not Allowed")
+		} else if c.Age > 12 && c.Age < 18 {
+			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Allowed but must come with someone above 21")
+		} else {
+			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Allowed")
+		}
 	}
 
-	for _, c := range customers {
-		fmt.Printf("ID: %d , Name: %s, Age : %d\n", c.ID, c.Name, c.Age)
-	}
 }
