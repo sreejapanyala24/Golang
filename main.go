23d7e2c89957f1f27cee3b46c833b1d3adb480d7
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-15 02:13:36 -0500

    Added logic to determine TicketPrice based on Age

--- main.go
+++ main.go
@@ -16,14 +16,19 @@ func main() {
 		{4, "Raj", 10},
 		{5, "Ravi", 18},
 	}
-	fmt.Printf("%-5s %-10s %-5s %-40s\n", "ID", "Name", "Age", "Status")
+	ActualTicketPrice := 15.0
+	fmt.Printf("%-5s %-10s %-5s %-45s %-10s\n", "ID", "Name", "Age", "Status", "TicketPrice")
 	for _, c := range customers {
-		if c.Age < 12 {
-			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Not Allowed")
-		} else if c.Age >= 12 && c.Age < 18 {
-			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Allowed but must come with someone above 21")
+		if c.Age < 18 {
+			TicketPrice := ActualTicketPrice / 2
+			if c.Age < 12 {
+				fmt.Printf("%-5d %-10s %-5d %-45s %-10s\n", c.ID, c.Name, c.Age, "Not Allowed", "NA")
+			} else if c.Age >= 12 && c.Age < 18 {
+				fmt.Printf("%-5d %-10s %-5d %-45s %-10.2f\n", c.ID, c.Name, c.Age, "Allowed but must come with someone above 21", TicketPrice)
+			}
 		} else {
-			fmt.Printf("%-5d %-10s %-5d %-40s\n", c.ID, c.Name, c.Age, "Allowed")
+			fmt.Printf("%-5d %-10s %-5d %-45s %-10.2f\n", c.ID, c.Name, c.Age, "Allowed", ActualTicketPrice)
+
 		}
 	}
 
