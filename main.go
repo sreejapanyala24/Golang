0d1a63dd36f490c9841b9b890fefe3cf5c4ec2a3
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-15 02:34:00 -0500

    Code Changes on New Requriment Task#1 and Task#2  (#2)
    * Removed Raj details
    
    * 4th Customer Details added
    
    * Add gitignore
    
    * Ignore Goland .idea folder
    
    * Added logic for Task1 - Allow users based on age
    
    * Added Edge case Scenario condition (age =12) and removed commented code
    
    * Added logic to determine TicketPrice based on Age

--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import "fmt"
 
-type Customer struct {
+type customer struct {
 	ID   int
 	Name string
 	Age  int
 }
 
 func main() {
-
-	customers := []Customer{
+	customers := []customer{
 		{1, "Sreeja", 25},
-		{2, "Bharath", 30},
+		{2, "Bharath", 12},
 		{3, "Sai", 28},
-		{4, "Raj", 27},
+		{4, "Raj", 10},
+		{5, "Ravi", 18},
+	}
+	ActualTicketPrice := 15.0
+	fmt.Printf("%-5s %-10s %-5s %-45s %-10s\n", "ID", "Name", "Age", "Status", "TicketPrice")
+	for _, c := range customers {
+		if c.Age < 18 {
+			TicketPrice := ActualTicketPrice / 2
+			if c.Age < 12 {
+				fmt.Printf("%-5d %-10s %-5d %-45s %-10s\n", c.ID, c.Name, c.Age, "Not Allowed", "NA")
+			} else if c.Age >= 12 && c.Age < 18 {
+				fmt.Printf("%-5d %-10s %-5d %-45s %-10.2f\n", c.ID, c.Name, c.Age, "Allowed but must come with someone above 21", TicketPrice)
+			}
+		} else {
+			fmt.Printf("%-5d %-10s %-5d %-45s %-10.2f\n", c.ID, c.Name, c.Age, "Allowed", ActualTicketPrice)
+
+		}
 	}
 
-	for _, c := range customers {
-		fmt.Printf("ID: %d , Name: %s, Age : %d\n", c.ID, c.Name, c.Age)
-	}
 }
